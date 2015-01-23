d8720d138cd28715308ecb3d7b7de41ebd1638b2
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2015-01-23 09:08:21 -0600

    Fix plot 3

--- plot3.R
+++ plot3.R
@@ -4,14 +4,11 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 NEI <- NEI[NEI$fips=="24510",]
-NEI <- left_join(NEI,SCC,by="SCC")
-NEI <- NEI[as.character(NEI$Data.Category) %in% c("Point","Onroad","Nonroad","Nonpoint"),]
 png("plot3.png")
-NEIgrouped <- group_by(NEI,year,Data.Category)
+NEIgrouped <- group_by(NEI,year,type)
 NEISummary <- summarize(NEIgrouped,totalEmission = sum(Emissions))
-p <- qplot(year, totalEmission,data = NEISummary,facets = .~Data.Category, geom=c("line"))
+p <- qplot(year, totalEmission,data = NEISummary,facets = .~type, geom=c("line"))
 p <- p+ggtitle("Total PM2.5 emission in Baltimore City from 1999-2008")
 p <- p+ylab("Total emission (tons)")
 print(p) 
 dev.off()
-
